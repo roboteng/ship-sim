38f19385a33d16f28815029beb3c7aaed05fd42b
Trevor Settles <roboteng@gmail.com>
2023-05-27 21:23:16 -0600

    aadded ship velocity

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
+const FORCE: f32 = 1.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(startup)
         .add_system(locations)
+        .add_system(update_velocity)
         .run();
 }
 
@@ -24,14 +27,36 @@ fn startup(
             ..default()
         },
         Ship,
+        Velocity::default(),
     ));
 }
 
-fn locations(mut locations: Query<&mut Transform, With<Ship>>) {
+fn locations(mut locations: Query<&mut Velocity, With<Ship>>, inputs: Res<Input<KeyCode>>) {
     for mut ship in locations.iter_mut() {
-        ship.translation.x += 1.0;
+        if inputs.pressed(KeyCode::W) {
+            ship.0.y += FORCE;
+        }
+        if inputs.pressed(KeyCode::A) {
+            ship.0.x -= FORCE;
+        }
+        if inputs.pressed(KeyCode::S) {
+            ship.0.y -= FORCE;
+        }
+        if inputs.pressed(KeyCode::D) {
+            ship.0.x += FORCE;
+        }
+    }
+}
+
+fn update_velocity(mut t: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+    for (mut pos, v) in t.iter_mut() {
+        pos.translation.x += v.0.x * FORCE * time.delta_seconds();
+        pos.translation.y += v.0.y * FORCE * time.delta_seconds();
     }
 }
 
 #[derive(Component)]
 struct Ship;
+
+#[derive(Component, Default)]
+struct Velocity(Vec2);
