6599c476df345c4181ecb6d3b0e72308b9a28fbf
Trevor Settles <roboteng@gmail.com>
2023-05-27 21:04:50 -0600

    moved shape

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(startup)
+        .add_system(locations)
+        .run();
 }
+
+fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Circle
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+            ..default()
+        },
+        Ship,
+    ));
+}
+
+fn locations(mut locations: Query<&mut Transform, With<Ship>>) {
+    for mut ship in locations.iter_mut() {
+        ship.translation.x += 1.0;
+    }
+}
+
+#[derive(Component)]
+struct Ship;
