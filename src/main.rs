d23a75e202ed57971870abfdb1647dfef48c11a7
Trevor Settles <roboteng@gmail.com>
2023-05-28 18:10:31 -0600

    rotated rudder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 const FORCE: f32 = 1.0;
+const RUDDER_TURN: f32 = 0.01;
 
 fn main() {
     App::new()
@@ -8,6 +9,7 @@ fn main() {
         .add_startup_system(startup)
         .add_system(locations)
         .add_system(update_velocity)
+        .add_system(apply_rudder_changes)
         .run();
 }
 
@@ -34,24 +36,29 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     visibility: Visibility::Visible,
                     ..default()
                 },
-                Rudder,
+                Rudder { angle: 0. },
             ));
         });
 }
 
-fn locations(mut locations: Query<&mut Velocity, With<Ship>>, inputs: Res<Input<KeyCode>>) {
+fn locations(
+    mut locations: Query<&mut Velocity, With<Ship>>,
+    mut rudder: Query<&mut Rudder>,
+    inputs: Res<Input<KeyCode>>,
+) {
+    let mut rudder = rudder.get_single_mut().unwrap();
     for mut ship in locations.iter_mut() {
         if inputs.pressed(KeyCode::W) {
             ship.0.y += FORCE;
         }
         if inputs.pressed(KeyCode::A) {
-            ship.0.x -= FORCE;
+            rudder.angle -= RUDDER_TURN;
         }
         if inputs.pressed(KeyCode::S) {
             ship.0.y -= FORCE;
         }
         if inputs.pressed(KeyCode::D) {
-            ship.0.x += FORCE;
+            rudder.angle += RUDDER_TURN;
         }
     }
 }
@@ -63,11 +70,20 @@ fn update_velocity(mut t: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
     }
 }
 
+fn apply_rudder_changes(mut rudders: Query<(&mut Transform, &Rudder)>) {
+    for (mut transform, rudder) in rudders.iter_mut() {
+        // transform.rotate_around(Vec3::splat(0.), Quat::from_rotation_z(rudder.angle));
+        transform.rotation = Quat::from_rotation_z(rudder.angle)
+    }
+}
+
 #[derive(Component)]
 struct Ship;
 
 #[derive(Component)]
-struct Rudder;
+struct Rudder {
+    angle: f32,
+}
 
 #[derive(Component, Default)]
 struct Velocity(Vec2);
